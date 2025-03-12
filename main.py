e194a8f16d0b2c1677068abb076ef3ade97329f1
JesperFahey <133820868+JesperFahey@users.noreply.github.com>
2025-03-13 10:01:11 +1300

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,18 @@
+'''
+Speeding Ticket Program
+Teukin Barauti
+Version 1
+This program gives out a ticket depending on how fast you are going.
+'''
+
+
 # Here are my Variables
 INT_LIMIT = 100
 STR_SPEEDINPUT = "How fast do you think you were going?"
+
+# If else statement
+if int_userSpeed > 100:
+  print(STR_UNSAFE)
 # Integers and stuff
 int_userSpeed = 0
 INT_MONEYOWED = 500
