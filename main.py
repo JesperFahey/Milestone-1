9cc77c2006e48d07c34814c777e6b458cfe028b0
JesperFahey <133820868+JesperFahey@users.noreply.github.com>
2025-03-13 09:50:23 +1300

    Update main.py
    Added 2 comments.

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 # Here are my Variables
 INT_LIMIT = 100
-STR_SPEEDINPUT = "How fast do you think you were going?" 
+STR_SPEEDINPUT = "How fast do you think you were going?"
+# Integers and stuff
 int_userSpeed = 0
 INT_MONEYOWED = 500
+# Strings and stuff! :D
 STR_SAFE = "Okay, sorry for the confusion."
 STR_GIMME_MONEY = "You give me ${} for speeding. Not good. Bad.".format(INT_MONEYOWED)
 STR_UNSAFE = "You could've died, slow down!"
